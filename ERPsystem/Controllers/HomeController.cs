b3ac9ac4b3d665bb5f56021072693f7a0bf14489
monika <monikakharmale55@gmail.com>
2025-04-23 09:17:51 +0530

    add a commit

--- ERPsystem/Controllers/HomeController.cs
+++ ERPsystem/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace ERPsystem.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //This is index page
             return View();
         }
         public ActionResult About()
