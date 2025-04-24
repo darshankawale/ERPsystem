856d810f72eb4a1aa5a9afac97c39979ae69887e
Darshan Kawale <tejalkawale14@gmail.com>
2025-04-24 23:01:18 +0530

    budget module

--- ERPsystem/Controllers/AdminController.cs
+++ ERPsystem/Controllers/AdminController.cs
@@ -12,18 +12,78 @@ namespace ERPsystem.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        ERPSYSTEMEntities ee = new ERPSYSTEMEntities();
         oprationdav oo= new oprationdav();  
         public ActionResult Index()
         {
 
 
-           object  a=  oo.list();
-            return View(a);
+           
+            return View();
+        }
+
+        public ActionResult Budgetshow()
+        {
+            var f = oo.showbudget();
+            
+            return View(f);  
         }
 
 
-       
+        public ActionResult Budgetadd() {
+        
+            budget b= new budget();
+        
+          return View(b);    
+        }
 
+        [HttpPost]
+        public ActionResult Budgetadd(budget b) {
+
+            int a = oo.budgetadd(b);
+            if (a == 0)
+            {
+
+
+                return RedirectToAction("Index");
+
+            }
+
+            return RedirectToAction("Budgetshow");
+            
+
+        }
+
+        public ActionResult editbudget(int id) { 
+        var  b = oo.editbudget(id);
+            ViewBag.Accounts = new SelectList(oo.getAccounts(), "account_id", "name", b.account_id);
+
+            return View(b);
+        }
+
+        [HttpPost]
+        public ActionResult editbudget(budget b)
+        {
+           
+                var result = oo.budgetupdate(b);
+                if (result == 1)
+                    return RedirectToAction("Budgetshow");
+          
+
+            ViewBag.Accounts = new SelectList(oo.getAccounts(), "account_id", "name", b.account_id);
+            return View(b);
+        }
+
+
+        public ActionResult deletebudget(int id)
+        {
+
+            int a= oo.deletebudget(id);
+            if (a == 1)
+                return RedirectToAction("Budgetshow");
+
+            return RedirectToAction("Budgetshow");
+        }
 
     }
 }
--- ERPsystem/Controllers/oprationdav.cs
+++ ERPsystem/Controllers/oprationdav.cs
@@ -11,13 +11,72 @@ namespace ERPsystem.Controllers
         ERPSYSTEMEntities ee= new ERPSYSTEMEntities();  
 
 
-       public object list()
+       public List<budget> showbudget()
         {
-           var f=  ee.transactions.ToList();
+           var f= ee.budgets.ToList();
             return f;
         }
 
+        public int budgetadd(budget b)
+        {
+            try
+            {
+                ee.budgets.Add(b);
+                ee.SaveChanges();
+                return 1;
+            }
+            catch (Exception ex) {
 
-       
+
+                return 0;
+
+            }
+        }
+
+        public budget editbudget(int id)
+        {
+            return ee.budgets.FirstOrDefault(x => x.budget_id == id);
+        }
+
+        public List<account> getAccounts()
+        {
+            return ee.accounts.ToList();
+        }
+
+
+        public int budgetupdate(budget b)
+        {
+
+          var f=   ee.budgets.FirstOrDefault(x => x.budget_id == b.budget_id);
+            f.spent = b.spent;
+            f.allocated = b.allocated;
+            f.account_id = b.account_id;
+            f.remaining = b.remaining;
+            f.fiscal_year = b.fiscal_year;
+            try
+            {
+                ee.budgets.AddOrUpdate(f);
+                ee.SaveChanges();
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public int deletebudget(int id)
+        {
+           var f=  ee.budgets.FirstOrDefault(x => x.budget_id == id);
+            if(f != null)
+            {
+                ee.budgets.Remove(f);
+                ee.SaveChanges();
+                return 1;
+            }
+            return 0;
+        }
+
+
     }
 }
